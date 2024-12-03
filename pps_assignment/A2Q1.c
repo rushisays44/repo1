aabc92e6e0ef94117c4537b8ce2ab9f16ca15ddb
rushisays44 <rushibandgar29@gmail.com>
2024-12-03 15:00:28 +0530

    A2Q1 third attempt

---
+++ pps_assignment/A2Q1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    inta;
+    printf("Enter a year");
+    sacnf("%",&a)
+    if(a%4==0)
+    {
+        printf("%d is a leap year")
+    }
+    else
+{
+    printf("%d is not a leap year") 
+}
